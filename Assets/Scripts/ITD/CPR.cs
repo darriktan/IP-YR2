a36815e6d09e89dbca15eb6c754fa86a9c271c15
MeAgan-chan <s10204841@connect.np.edu.sg>
2022-02-09 17:47:24 +0800

    Added CPR and Questing scripts

--- Assets/Scripts/ITD/CPR.cs
+++ Assets/Scripts/ITD/CPR.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class CPR : MonoBehaviour
 {
+    //public GameObject collider1;
+    //public GameObject collider2;
+
+    public int collisionCount = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("oh man");
+        if (other.gameObject.tag == "FirstCollider")
+        {
+            collisionCount += 1;
+            Debug.Log("First Collider" + other.gameObject.name);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/ITD/Questing.cs
+++ Assets/Scripts/ITD/Questing.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Snapping : MonoBehaviour
+public class Questing : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
